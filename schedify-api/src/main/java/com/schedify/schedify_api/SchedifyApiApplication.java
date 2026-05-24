afc75460d712edc5e9e5fe952f25a6b7c3cc5a0e
Edson Sousa <edson3711.es@gmail.com>
2026-05-24 16:57:10 -0300

    primeiro commit

---
+++ schedify-api/src/main/java/com/schedify/schedify_api/SchedifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.schedify.schedify_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedifyApiApplication.class, args);
+	}
+
+}
